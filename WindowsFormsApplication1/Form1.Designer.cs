092be6f48252127bd053954d2d2634cfcf55a6fb
ObjectMan3 <44414288+ObjectMan3@users.noreply.github.com>
2019-11-16 08:59:15 -0600

    First commit for publishing
    This "First commit for publishing" contains a MS Visual Studio C# Project in a Solution of an application I made that Displays Information on Network Adapters Installed and Status Real-Time. Double click the dialog to close it.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.AccessibleName = "";
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.CausesValidation = false;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox1.Enabled = false;
+            this.listBox1.Font = new System.Drawing.Font("Eras Demi ITC", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.IntegralHeight = false;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Location = new System.Drawing.Point(265, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox1.Size = new System.Drawing.Size(117, 160);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.Black;
+            this.listBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox2.CausesValidation = false;
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox2.Enabled = false;
+            this.listBox2.Font = new System.Drawing.Font("Eras Demi ITC", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.ForeColor = System.Drawing.Color.White;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.IntegralHeight = false;
+            this.listBox2.ItemHeight = 14;
+            this.listBox2.Location = new System.Drawing.Point(0, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox2.Size = new System.Drawing.Size(266, 160);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(382, 160);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Enabled = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Network Connections";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Net;
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            FormWindowState previousWindowState = this.WindowState;
+
+            base.WndProc(ref m);
+
+            FormWindowState currentWindowState = this.WindowState;
+
+            if (previousWindowState != currentWindowState && currentWindowState == FormWindowState.Maximized)
+            {
+                this.Close();
+            }
+            switch (m.Msg)
+            {
+                case WM_NCHITTEST:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == HTCLIENT)
+                        m.Result = (IntPtr)HTCAPTION;
+                    return;
+             }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.SendToBack();
+            listBox2.SendToBack();
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            this.SizeChanged += new EventHandler(Form1_SizeChanged);
+            FormMaximized += new EventHandler(Form1_FormMaximized);
+
+            _CurrentWindowState = this.WindowState;
+            if (_CurrentWindowState == FormWindowState.Maximized)
+            {
+                this.Close();
+            }
+        }
+
+        private void FillUps()
+        {
+            listBox2.Items.Clear();
+            listBox1.Items.Clear();
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            listBox2.Items.Add(" ");
+            listBox1.Items.Add(" ");
+            foreach (NetworkInterface adapter in interfaces)
+            {
+                var ipProps = adapter.GetIPProperties();
+
+                foreach (var ip in ipProps.UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        listBox2.Items.Add(adapter.Description.ToString());
+                        if (adapter.OperationalStatus.ToString() == "Up")
+                        { listBox1.Items.Add(ip.Address.ToString()); }
+                        else { listBox1.Items.Add(adapter.OperationalStatus.ToString()); }
+                    }
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FillUps();
+        }
+        public event EventHandler FormMaximized;
+        private void FireFormMaximized()
+        {
+            if (FormMaximized != null)
+            {
+                FormMaximized(this, EventArgs.Empty);
+            }
+        }
+
+        private FormWindowState _CurrentWindowState;
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized && _CurrentWindowState != FormWindowState.Maximized)
+            {
+                FireFormMaximized();
+            }
+            _CurrentWindowState = this.WindowState;
+        }
+
+        void Form1_FormMaximized(object sender, EventArgs e)
+        {
+        
+            this.Close();
+        }
+        private void Form1_Maximize(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            this.Close();
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void ListBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            FillUps();
+            listBox1.Refresh();
+        }
+        
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox1_mouseclick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            FillUps();
+            listBox1.Refresh();
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
